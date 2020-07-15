0be5ce06e03f446cbd82141003d3638e3f7fc28a
Azat Alekbaev <alekbaev1@icloud.com>
2020-07-15 15:16:30 +0300

    Splash screen added

---
+++ SimbirSoftTasksAlekbaev/SplashScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashScreenViewController.swift
+//  SimbirSoftTasksAlekbaev
+//
+//  Created by Азат Алекбаев on 15.07.2020.
+//  Copyright © 2020 Азат Алекбаев. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
