6450cee17bf3bf4fd4e6c8e0327cd4a30e4fef9d
Azat Alekbaev <alekbaev1@icloud.com>
2020-07-18 17:47:53 +0300

    Final changes added

--- SimbirSoftTasksAlekbaev/HelpViewController.swift
+++ SimbirSoftTasksAlekbaev/HelpViewController.swift
@@ -16,6 +16,11 @@ class HelpViewController: UIViewController, UICollectionViewDelegate, UICollecti
         super.viewDidLoad()
         let nib = UINib(nibName: "HelpCollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "helpCellIdentifier")
+        self.navigationItem.title = "Помочь"
+        navigationController?.navigationBar.barTintColor = UIColor(red: 117.0/255.0, green: 163.0/255.0, blue: 71.0/255.0, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        collectionView.backgroundColor = .white
+        tabBarController?.tabBar.barTintColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
         
     }
     
@@ -30,9 +35,27 @@ class HelpViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return cell
     }
     
+    var cellWidth:CGFloat = 0
+        var cellHeight:CGFloat = 0
+        var spacing:CGFloat = 5
+        var numberOfColumn:CGFloat = 2
+
+
+        //MARK:-LifeCycle
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+
+            collectionView.contentInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+
+            if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout{
+                cellWidth = (collectionView.frame.width  / 2) - spacing * 4
+               cellHeight = cellWidth
+                flowLayout.minimumLineSpacing = spacing
+                flowLayout.minimumInteritemSpacing = spacing
+            }
+        }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 174, height: 160)
+        return CGSize(width: cellWidth, height: cellHeight)
     }
-    
-    
 }
